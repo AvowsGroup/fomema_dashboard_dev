0e64b44eeaeac7b7395401f609f5e6410bca6c22
yuvraj <yuvraj.s@avowstech.com>
2023-10-27 16:38:11 +0530

    kpi queries

--- app/controllers/third_dashboard_controller.rb
+++ app/controllers/third_dashboard_controller.rb
@@ -1,4 +1,6 @@
 class ThirdDashboardController < ApplicationController
+  before_action :initialize_date_range, only: [:index, :kpi_percentage, :review_xray, :pcr_xray]
+
   def index
     @task_list = tasks
     result_key
@@ -12,6 +14,12 @@ class ThirdDashboardController < ApplicationController
     @task_list = tasks
     result_key
     @users = User.select(:name, :designation).paginate(page: page_number, per_page: 10)
+  end
+
+  def initialize_date_range
+    @current_date = Date.today
+    @beginning_of_year = Date.new(@current_date.year, 1, 1).to_s
+    @end_of_year = Date.new(@current_date.year, 12, 31).to_s
   end
 
   private
@@ -35,19 +43,19 @@ class ThirdDashboardController < ApplicationController
     @params ||= {}
     [
       {
-        KPI: 0, # as there is no table mentioned in the doc
+        KPI: 0, # yet to be discussed
         task: 'Email',
         TAT: 'First response time within 24 business hours',
         target: '80%'
       },
       {
-        KPI: 0, # as there is no table mentioned in the doc so pasting 0 in value
+        KPI: 0, # yet to be discussed
         task: 'Chat',
         TAT: 'First response time within 24 business hours ',
         target: '80%'
       },
       {
-        KPI: 0,
+        KPI: 0, # yet to be discussed
         task: 'Bypass fingerprint approval',
         TAT: '24 business hours',
         target: '80%'
@@ -65,7 +73,7 @@ class ThirdDashboardController < ApplicationController
         target: '80%'
       },
       {
-        KPI: kpi_percentage("ApprovalRequest", 3),
+        KPI: 0, # yet to be discussed
         task: 'Foreign Worker Amendment',
         TAT: '3wd',
         target: '80%'
@@ -101,67 +109,67 @@ class ThirdDashboardController < ApplicationController
         target: '80%'
       },
       {
-        KPI: 0,
+        KPI: 0, # yet to be discussed
         task: 'XQCC Amendment',
         TAT: 'Four (4) weeks from the date of confirmation',
         target: '80%'
       },
       {
-        KPI: 0, # as there is no table menthioned in the doc
+        KPI: kpi_percentage("Laboratory", nil),
         task: 'Laboratory',
         TAT: 'Calendar year',
         target: '100%'
       },
       {
-        KPI: 0, # as there is no table menthioned in the doc
+        KPI: kpi_percentage("DoctorVisit", nil),
         task: 'Doctor Visit',
         TAT: 'Calendar year',
         target: '100%'
       },
       {
-        KPI: 0, # as there is no table menthioned in the doc
+        KPI: kpi_percentage("XrayFacility", nil),
         task: 'X-ray Visit',
         TAT: 'Calendar year',
         target: '100%'
       },
       {
-        KPI: 0,
+        KPI: 0, # yet to be discussed
         task: 'Payment to service providers (Doctor, X-ray, Laboratory)',
         TAT: 'For every 7 working days, Finance team will generate data from Nios and transmit those data to Sage, our accounting system, to perform the payment processes.\n\nThe payments will be generated into 5 batches based on the certification dates:\n\na) 1st - 6th\nb) 7th - 12th\nc) 13th -18th\nd) 19th -24th\ne) 25th -30th',
         target: '100%'
       },
       {
-        KPI: 0,
+        KPI: 0, # yet to be discussed
         task: 'Refund to Employers',
         TAT: '80%',
         target: ''
       },
       {
-        KPI: 0,
+        KPI: 0, # yet to be discussed
         task: 'Insurance payment to Fomema Global Sdn Bhd',
         TAT: '100%',
         target: '80%'
       },
       {
-        KPI: 0,
+        KPI: 0, # yet to be discussed
         task: 'Appeal cases',
         TAT: '28wd',
         target: '90%'
       },
       {
-        KPI: 0,
+        KPI: 0, # yet to be discussed
         task: 'Pending Review Cases',
         TAT: '3wd',
         target: '90%'
       },
       {
-        KPI: 0,
+        KPI: 0, # yet to be discussed
         task: 'TCUPI Cases',
         TAT: '28wd',
         target: '90%'
       },
       {
-        KPI: 0, # data from CS new system
+        KPI: 0, # yet to be discussed
         task: 'Employer Enquiry JIVO',
         TAT: '24 hours',
         target: '100%'
@@ -197,19 +205,19 @@ class ThirdDashboardController < ApplicationController
         target: '80%'
       },
       {
-        KPI: 0, # Avows System (Claims Management)
+        KPI: 0, # yet to be discussed
         task: 'Staff Claims Submission',
         TAT: 'One Week from date HODs approved to HCM verification',
         target: '90%'
       },
       {
-        KPI: 0,
+        KPI: kpi_percentage("ApprovalandActivation", 14),
         task: 'Approval for registration of service provider',
         TAT: '14wd',
         target: '80%'
       },
       {
-        KPI: 0,
+        KPI: kpi_percentage("ApprovalandActivation", 10),
         task: 'Activating new service provider',
         TAT: '10wd',
         target: '80%'
@@ -220,6 +228,14 @@ class ThirdDashboardController < ApplicationController
   def kpi_percentage(model_name, tat)
     achieved_count = 0
     not_achieved_count = 0
+    total_count = 0
+
+    if params[:dateRange].present?
+      start_date, end_date = params[:dateRange].split(" - ").map { |date| Date.strptime(date, "%d/%m/%Y") }
+    else
+      start_date = @beginning_of_year
+      end_date = @end_of_year
+    end
 
     case model_name
     when "Employer"
@@ -238,9 +254,36 @@ class ThirdDashboardController < ApplicationController
       achieved_count = FwChangeEmployer.where("approval_at <= requested_at + interval '? days'", tat).count
       not_achieved_count = FwChangeEmployer.where("approval_at > requested_at + interval '? days'", tat).count
 
+    when "Laboratory"
+      total_active = Laboratory.where(status: 'ACTIVE').count
+      total_visit = VisitReport.where(visit_date: start_date..end_date).count
+
+    when "DoctorVisit"
+      total_active = Doctor.where(status: 'ACTIVE').count
+      total_visit = VisitReport.where(visit_date: start_date..end_date).count
+
+    when "XrayFacility"
+      total_active = XrayFacility.where(status: 'ACTIVE').count
+      total_visit = VisitReport.where(visit_date: start_date..end_date).count
+
+    when "ApprovalandActivation"
+      doctor_achieved = Doctor.where("registration_approved_at <= created_at + interval '? days'", tat).count
+      doctor_not_achieved = Doctor.where("registration_approved_at > created_at + interval '? days'", tat).count
+      xray_achieved = XrayFacility.where("registration_approved_at <= created_at + interval '? days'", tat).count
+      xray_not_achieved = XrayFacility.where("registration_approved_at > created_at + interval '? days'", tat).count
+      lab_achived = Laboratory.where("registration_approved_at <= created_at + interval '? days'", tat).count
+      lab_not_achieved = Laboratory.where("registration_approved_at > created_at + interval '? days'", tat).count
+      achieved_count = doctor_achieved + xray_achieved + lab_achived
+      not_achieved_count = doctor_not_achieved + xray_not_achieved + lab_not_achieved
     end
 
-    total_count = achieved_count + not_achieved_count
+    if model_name == "Laboratory" || model_name == "DoctorVisit" || model_name == "XrayFacility"
+      achieved_count = total_visit
+      total_count = total_active
+    else
+      total_count = achieved_count + not_achieved_count
+    end
+
     kpi_percentage = total_count > 0 ? (achieved_count.to_f / total_count) * 100 : 0
     kpi_percentage.round(1) # Round to one decimal place
   end
@@ -249,15 +292,23 @@ class ThirdDashboardController < ApplicationController
     achieved_count = 0
     not_achieved_count = 0
 
-    achieved_count = Transaction.joins("INNER JOIN xray_reviews ON xray_reviews.id = transactions.xray_review_id")
-                                .where("xray_reviews.transmitted_at <= transactions.certification_date + INTERVAL '3' DAY")
-                                .where("transactions.transaction_date BETWEEN ? AND ?", '2023-01-01', '2023-12-31')
-                                .count
+    if params[:dateRange].present?
+      start_date, end_date = params[:dateRange].split(" - ").map { |date| Date.strptime(date, "%d/%m/%Y") }
+    else
+      start_date = @beginning_of_year
+      end_date = @end_of_year
+    end
 
-    not_achieved_count = Transaction.joins("INNER JOIN xray_reviews ON xray_reviews.id = transactions.xray_review_id")
-                                    .where("xray_reviews.transmitted_at > transactions.certification_date + INTERVAL '3' DAY")
-                                    .where("transactions.transaction_date BETWEEN ? AND ?", '2023-01-01', '2023-12-31')
-                                    .count
+    #
+    # achieved_count = Transaction.joins("INNER JOIN xray_reviews ON xray_reviews.id = transactions.xray_review_id")
+    #                             .where("xray_reviews.transmitted_at <= transactions.certification_date + INTERVAL '3' DAY")
+    #                             .where(transactions: { transaction_date: start_date..end_date })
+    #                             .count
+    #
+    # not_achieved_count = Transaction.joins("INNER JOIN xray_reviews ON xray_reviews.id = transactions.xray_review_id")
+    #                                 .where("xray_reviews.transmitted_at > transactions.certification_date + INTERVAL '3' DAY")
+    #                                 .where(transactions: { transaction_date: start_date..end_date })
+    #                                 .count
 
     total_count = achieved_count + not_achieved_count
     kpi_percentage = total_count > 0 ? (achieved_count.to_f / total_count) * 100 : 0
@@ -269,20 +320,50 @@ class ThirdDashboardController < ApplicationController
     achieved_count = 0
     not_achieved_count = 0
 
+    if params[:dateRange].present?
+      start_date, end_date = params[:dateRange].split(" - ").map { |date| Date.strptime(date, "%d/%m/%Y") }
+    else
+      start_date = @beginning_of_year
+      end_date = @end_of_year
+    end
+
     achieved_count = PcrReview.joins("INNER JOIN transactions ON transactions.pcr_review_id = pcr_reviews.id")
                               .where("pcr_reviews.transmitted_at <= (transactions.certification_date + INTERVAL '2 days') AT TIME ZONE 'UTC'")
-                              .where(transactions: { transaction_date: '2023-01-01'..'2023-12-31' })
+                              .where(transactions: { transaction_date: start_date..end_date })
                               .count
 
     not_achieved_count = PcrReview.joins("INNER JOIN transactions ON transactions.pcr_review_id = pcr_reviews.id")
                                   .where("pcr_reviews.transmitted_at > (transactions.certification_date + INTERVAL '2 days') AT TIME ZONE 'UTC'")
-                                  .where(transactions: { transaction_date: '2023-01-01'..'2023-12-31' })
+                                  .where(transactions: { transaction_date: start_date..end_date })
                                   .count
 
     total_count = achieved_count + not_achieved_count
     kpi_percentage = total_count > 0 ? (achieved_count.to_f / total_count) * 100 : 0
-    kpi_percentage.round(1) # Round to one decimal place
+    kpi_percentage.round(1)
 
+  end
+
+  def filter_params(query = {}, records = 0)
+    records = records
+    query.each do |key, value|
+      case key
+      when "dateRange"
+        start_date, end_date = value.split(" - ").map { |date| Date.strptime(date, "%d/%m/%Y") }
+        records = records.where(created_at: start_date.beginning_of_day..end_date.end_of_day)
+      when "month"
+        unless value == "Select Monthly"
+          current_year = Date.today.year
+          selected_month = Date::MONTHNAMES.index(value.split(" ").last)
+          records = records.where("EXTRACT(MONTH FROM created_at) = ? AND EXTRACT(YEAR FROM created_at) = ?", selected_month, current_year)
+        end
+        # when "week"
+        #   unless value == "Select Week"
+        #     selected_week = value.split(" ").last.to_i
+        #     records = records.where("EXTRACT(WEEK FROM created_at) = ?", selected_week)
+        #   end
+      end
+    end
+    records
   end
 
 end
