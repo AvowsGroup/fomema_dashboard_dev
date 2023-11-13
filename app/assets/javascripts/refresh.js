b1eea346f74496a85f2a52c48b96c0cafeb11164
yuvraj <yuvraj.s@avowstech.com>
2023-11-13 13:31:21 +0530

    refresh function included

--- app/assets/javascripts/refresh.js
+++ app/assets/javascripts/refresh.js
@@ -1,12 +1,50 @@
-var count = 0;
-
-function setReloadInterval(interval) {
-    clearInterval(myInterval); // Clear any previously set intervals
-    myInterval = setInterval(function () {
-        count++; // Increment the count
-        console.log("Count: " + count); // Display count in the console
-        window.location.reload(); // Refresh the page
-    }, interval * 1000); // Convert seconds to milliseconds
+// Function to perform a hard refresh
+function hardRefresh() {
+    location.reload(true); // Pass true to perform a hard refresh
 }
 
-var myInterval; // Declare a variable to store the interval
+// testing
+// Set up a click event listener for the refresh button
+document.getElementById('refreshButton').addEventListener('click', function () {
+    // Clear existing intervals to prevent multiple timers
+    clearInterval(window.refreshInterval);
+
+    // Set up a timer to call the hardRefresh function every 5 seconds
+    window.refreshInterval = setInterval(hardRefresh, 5000);  // 5000 milliseconds = 5 seconds
+
+    // Perform a hard refresh after the initial 5 seconds
+    setTimeout(hardRefresh, 5000); // 5000 milliseconds = 5 seconds
+});
+
+// Perform the first refresh after 5 seconds
+setTimeout(hardRefresh, 5000); // 5000 milliseconds = 5 seconds
+// testing
+
+// Set up a click event listener for the 5-minute refresh button
+document.getElementById('refreshButton5').addEventListener('click', function () {
+
+    // Clear existing intervals to prevent multiple timers
+    clearInterval(window.refreshInterval);
+
+    // Set up a timer to call the hardRefresh function every 5 minutes
+    window.refreshInterval = setInterval(hardRefresh, 300000); // 300000 milliseconds = 5 minutes
+
+    // Perform a hard refresh after the initial 5 minutes
+    setTimeout(hardRefresh, 300000); // 300000 milliseconds = 5 minutes
+});
+
+// Perform the first refresh after 5 Minutes
+setTimeout(hardRefresh, 300000);
+
+
+// Set up a click event listener for the 10-minute refresh button
+document.getElementById('refreshButton10').addEventListener('click', function () {
+    // Set up a timer to call the hardRefresh function every 10 minutes
+    setInterval(hardRefresh, 600000); // 600000 milliseconds = 10 minutes
+
+    // Perform a hard refresh after the initial 10 minutes
+    window.refreshInterval = setTimeout(hardRefresh, 600000); // 600000 milliseconds = 10 minutes
+});
+
+// Perform the first refresh after 10 Minutes
+setTimeout(hardRefresh, 600000);
