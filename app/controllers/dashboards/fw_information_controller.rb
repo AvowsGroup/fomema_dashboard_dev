42d249f4862580ee91bf963a0bfd2cbd8b37094e
yuvraj <yuvraj.s@avowstech.com>
2023-11-30 16:02:18 +0530

    removed harcoding of state codes

--- app/controllers/dashboards/fw_information_controller.rb
+++ app/controllers/dashboards/fw_information_controller.rb
@@ -27,8 +27,6 @@ class Dashboards::FwInformationController < ApplicationController
         @transaction_line_chart = @transactions.transaction_data_last_5_years rescue {}
         @sums_by_year = @transaction_line_chart.transform_values { |data| data.sum }
 
-
-
         # filtered data for data_points
         @passed_examination_count = @transactions.where("EXTRACT(YEAR FROM medical_examination_date) = ? AND medical_examination_date < ?", @current_year, Date.current).count
         @certification_count = @transactions.where("EXTRACT(YEAR FROM certification_date) = ? AND certification_date < ?", @current_year, Date.current).count
@@ -41,10 +39,11 @@ class Dashboards::FwInformationController < ApplicationController
         state_ids = @transactions.joins(doctor: :state).pluck('states.id')
         hash = {}
         state_ids.sort.uniq.each { |h| hash[h] = state_ids.count(h) }
-        state_names = State.where(id: state_ids.sort.uniq).pluck(:name)
+        state_names = State.where(id: state_ids.sort.uniq).pluck(:name, :long_code)
         converted_hash = {}
         state_names.each_with_index { |value, index| converted_hash[value] = hash.values[index] }
-        @fw_reg_by_states = converted_hash.to_a
+        @fw_reg_by_states = converted_hash.map { |key, value| key + [value] }
+
 
         @fw_Reg_by_countries = Transaction
                                  .joins(:country)
@@ -58,7 +57,7 @@ class Dashboards::FwInformationController < ApplicationController
       @side_bar_medical_appeals = Transaction.includes(:medical_appeals).find(@transactions.ids).count
       @pi_chart_data = [['Task', 'Hours per Day']]
       Transaction.joins(:job_type).group('job_types.name').count.to_a.map { |i| @pi_chart_data << i }
-      @fw_reg_by_states = State.joins(doctors: :transactions).group('states.name').count.to_a
+      @fw_reg_by_states = State.joins(doctors: :transactions).group('states.name', 'states.long_code').pluck('states.name', 'states.long_code', 'COUNT(*)').map { |name, long_code, count| [name, long_code, count] }
 
       @fw_Reg_by_countries = Transaction
                                .joins(:country)
@@ -75,7 +74,7 @@ class Dashboards::FwInformationController < ApplicationController
       last_five_years = (current_year - 4..current_year)
 
       @transaction_line_chart = last_five_years.each_with_object({}) do |year, chart_data|
-        chart_data[year] = [0] * 12  # Initialize an array of zeros for each month
+        chart_data[year] = [0] * 12 # Initialize an array of zeros for each month
       end
     end
 
@@ -512,9 +511,7 @@ class Dashboards::FwInformationController < ApplicationController
 
     @latest_transactions = Transaction.order(created_at: :desc).limit(50).pluck(:created_at).map { |date| date.strftime("%Y-%m-%d %H:%M:%S") }
 
-
     current_year = Date.current.year
-
 
     (0..4).each do |i|
       year = current_year - i
@@ -529,7 +526,6 @@ class Dashboards::FwInformationController < ApplicationController
       instance_variable_set("@raw_data_#{year}", raw_data_for_year)
     end
 
-
     @sheet_data = generate_dynamic_sheet_data
 
     respond_to do |format|
@@ -541,156 +537,156 @@ end
 
 private
 
-  def generate_dynamic_sheet_data
-    current_year = Time.now.year
-    years = (current_year.downto(current_year - 4)).to_a
+def generate_dynamic_sheet_data
+  current_year = Time.now.year
+  years = (current_year.downto(current_year - 4)).to_a
 
-    sheet_data = {}
+  sheet_data = {}
 
-    sheet_data['FW Reg. by Country'] = generate_sheet_data_for_category('FW Registration by Country', years)
-    sheet_data['FW Reg. by State'] = generate_sheet_data_for_category('FW Registration by State', years)
-    sheet_data['FW Reg. by Sector'] = generate_sheet_data_for_category('FW Registration by Sector', years)
-    sheet_data['FW Reg. by Gender'] = generate_sheet_data_for_category('FW Registration by Gender', years)
-    sheet_data['FW Reg. by Registration at'] = generate_sheet_data_for_category('FW Registration by Registration at', years)
-    sheet_data['FW Reg. by FW Type'] = generate_sheet_data_for_category('FW Registration by FW Type', years)
-    sheet_data['Trend of FW Reg. by year'] = ['Trend of FW registration by Year', ['Transaction date by Month', 'Transaction date by Day'] + years.map(&:to_s) + ['Count']]
+  sheet_data['FW Reg. by Country'] = generate_sheet_data_for_category('FW Registration by Country', years)
+  sheet_data['FW Reg. by State'] = generate_sheet_data_for_category('FW Registration by State', years)
+  sheet_data['FW Reg. by Sector'] = generate_sheet_data_for_category('FW Registration by Sector', years)
+  sheet_data['FW Reg. by Gender'] = generate_sheet_data_for_category('FW Registration by Gender', years)
+  sheet_data['FW Reg. by Registration at'] = generate_sheet_data_for_category('FW Registration by Registration at', years)
+  sheet_data['FW Reg. by FW Type'] = generate_sheet_data_for_category('FW Registration by FW Type', years)
+  sheet_data['Trend of FW Reg. by year'] = ['Trend of FW registration by Year', ['Transaction date by Month', 'Transaction date by Day'] + years.map(&:to_s) + ['Count']]
 
-    years.each do |year|
-      sheet_name = "Raw Data #{year}"
-      title = "Data #{year}"
-      headers = [
-        'Transaction Date (Month)', 'Medical Examination Date (Month)', 'Certification Date (Month)',
-        'State', 'Country', 'Age', 'Gender', 'Registration at', 'Foreign Worker Type',
-        'XQCC Pool (Film Received)', 'XQCC Pool (Film Reviewed)',
-        'PCR Pool (Film Received)', 'PCR Pool (Film Reviewed)',
-        'X-Ray Pending Review (Film Received)', 'X-Ray Pending Review (Film Reviewed)',
-        'X-Ray Pending Decision (Film Received)', 'X-Ray Pending Decision (Film Reviewed)',
-        'Medical Review (Received)', 'Medical Review (Reviewed)',
-        'Final Result Released', 'Result Transmitted to Immigration',
-        'Blocked FW', 'Appeal', 'FW Insured'
-      ]
+  years.each do |year|
+    sheet_name = "Raw Data #{year}"
+    title = "Data #{year}"
+    headers = [
+      'Transaction Date (Month)', 'Medical Examination Date (Month)', 'Certification Date (Month)',
+      'State', 'Country', 'Age', 'Gender', 'Registration at', 'Foreign Worker Type',
+      'XQCC Pool (Film Received)', 'XQCC Pool (Film Reviewed)',
+      'PCR Pool (Film Received)', 'PCR Pool (Film Reviewed)',
+      'X-Ray Pending Review (Film Received)', 'X-Ray Pending Review (Film Reviewed)',
+      'X-Ray Pending Decision (Film Received)', 'X-Ray Pending Decision (Film Reviewed)',
+      'Medical Review (Received)', 'Medical Review (Reviewed)',
+      'Final Result Released', 'Result Transmitted to Immigration',
+      'Blocked FW', 'Appeal', 'FW Insured'
+    ]
 
-      sheet_data[sheet_name] = [title, headers]
+    sheet_data[sheet_name] = [title, headers]
+  end
+
+  sheet_data
+end
+
+def generate_sheet_data_for_category(category_name, years)
+  [category_name, [category_name, 'Total FW Registration', 'FW went for medical examination', 'Certification', 'XQCC Pool (Film Received)', 'XQCC Pool (Film Reviewed)', 'PCR Pool (Film Received)', 'PCR Pool (Film Reviewed)', 'X-Ray Pending Review (Film Received)', 'X-Ray Pending Review (Film Reviewed)', 'X-Ray Pending Decision (Film Received)', 'X-Ray Pending Decision (Film Reviewed)', 'Medical Review (Received)', 'Medical Review (Reviewed)', 'Final Result Released', 'Result Transmitted to Immigration', 'Blocked FW', 'Appeal', 'FW Insured']]
+end
+
+def convert_values_to_arrays(hash)
+  converted_hash = {}
+
+  hash.each_with_index do |(key, value), index|
+    if index == 0
+      converted_hash[key] = value
+    else
+      converted_hash[key] = value.split(' ')
     end
-
-    sheet_data
   end
 
-  def generate_sheet_data_for_category(category_name, years)
-    [category_name, [category_name, 'Total FW Registration', 'FW went for medical examination', 'Certification', 'XQCC Pool (Film Received)', 'XQCC Pool (Film Reviewed)', 'PCR Pool (Film Received)', 'PCR Pool (Film Reviewed)', 'X-Ray Pending Review (Film Received)', 'X-Ray Pending Review (Film Reviewed)', 'X-Ray Pending Decision (Film Received)', 'X-Ray Pending Decision (Film Reviewed)', 'Medical Review (Received)', 'Medical Review (Reviewed)', 'Final Result Released', 'Result Transmitted to Immigration', 'Blocked FW', 'Appeal', 'FW Insured']]
-  end
+  converted_hash
+end
 
-  def convert_values_to_arrays(hash)
-    converted_hash = {}
-
-    hash.each_with_index do |(key, value), index|
-      if index == 0
-        converted_hash[key] = value
-      else
-        converted_hash[key] = value.split(' ')
+def apply_filter(filter_params)
+  transactions = Transaction.all
+  filter_params.each do |param_key, param_value|
+    case param_key
+    when "DateRange"
+      if param_value.present?
+        start_date, end_date = param_value.split(" - ")
+        transactions = transactions.where(created_at: start_date..end_date)
       end
-    end
-
-    converted_hash
-  end
-
-  def apply_filter(filter_params)
-    transactions = Transaction.all
-    filter_params.each do |param_key, param_value|
-      case param_key
-      when "DateRange"
-        if param_value.present?
-          start_date, end_date = param_value.split(" - ")
-          transactions = transactions.where(created_at: start_date..end_date)
-        end
-      when "Sector"
-        if param_value.present?
-          sector_names = JobType.pluck(:name)
-          selected_sector_names = sector_names & param_value
-          transactions = transactions.joins(:job_type).where("job_types.name" => selected_sector_names)
-        end
-      when "Country"
-        if param_value.present?
-          transactions = transactions.joins(:country).where("countries.name" => param_value)
-        end
-      when "State"
-        if param_value.present?
-          state_ids = State.where(name: param_value).pluck(:id)
-          doctor_ids = Doctor.where(state_id: state_ids).pluck(:id)
-          transactions = transactions.where(doctor_id: doctor_ids)
-        end
-      when "Gender"
-        if param_value.present?
-          transactions = transactions.where(fw_gender: param_value)
-        end
-      when "age"
-        if param_value.present?
-          age_ranges = param_value.map do |age_range|
-            if age_range == "65+"
-              [65, nil] # Represent "65+" as [65, nil]
-            else
-              age_range.split("-").map(&:to_i)
-            end
-          end
-
-          birth_years = age_ranges.map do |min_age, max_age|
-            if max_age.nil?
-              birth_year_min = Date.today.year - 65
-              birth_year_max = nil # Handle "65+" case with no maximum age
-            else
-              birth_year_min = Date.today.year - max_age - 1
-              birth_year_max = Date.today.year - min_age
-            end
-            [birth_year_min, birth_year_max]
-          end
-
-          # Find the overall minimum and maximum birth years
-          overall_birth_year_min = birth_years.map(&:first).min
-          overall_birth_year_max = birth_years.map(&:last).max
-
-          transactions = transactions.where(fw_date_of_birth: Date.new(overall_birth_year_min)..Date.new(overall_birth_year_max))
-        end
-
-      when "ForeginWorker"
-        if param_value.present?
-          transactions = transactions.where(registration_type: param_value)
-        end
-      when "Registration"
-        if param_value.present?
-          organization_names = Organization.pluck(:name).uniq
-          selected_organization_names = organization_names & param_value
-          transactions = transactions.joins(:organization).where("organizations.name" => selected_organization_names)
-        end
-        # Add more cases for other filter keys here
+    when "Sector"
+      if param_value.present?
+        sector_names = JobType.pluck(:name)
+        selected_sector_names = sector_names & param_value
+        transactions = transactions.joins(:job_type).where("job_types.name" => selected_sector_names)
       end
+    when "Country"
+      if param_value.present?
+        transactions = transactions.joins(:country).where("countries.name" => param_value)
+      end
+    when "State"
+      if param_value.present?
+        state_ids = State.where(name: param_value).pluck(:id)
+        doctor_ids = Doctor.where(state_id: state_ids).pluck(:id)
+        transactions = transactions.where(doctor_id: doctor_ids)
+      end
+    when "Gender"
+      if param_value.present?
+        transactions = transactions.where(fw_gender: param_value)
+      end
+    when "age"
+      if param_value.present?
+        age_ranges = param_value.map do |age_range|
+          if age_range == "65+"
+            [65, nil] # Represent "65+" as [65, nil]
+          else
+            age_range.split("-").map(&:to_i)
+          end
+        end
+
+        birth_years = age_ranges.map do |min_age, max_age|
+          if max_age.nil?
+            birth_year_min = Date.today.year - 65
+            birth_year_max = nil # Handle "65+" case with no maximum age
+          else
+            birth_year_min = Date.today.year - max_age - 1
+            birth_year_max = Date.today.year - min_age
+          end
+          [birth_year_min, birth_year_max]
+        end
+
+        # Find the overall minimum and maximum birth years
+        overall_birth_year_min = birth_years.map(&:first).min
+        overall_birth_year_max = birth_years.map(&:last).max
+
+        transactions = transactions.where(fw_date_of_birth: Date.new(overall_birth_year_min)..Date.new(overall_birth_year_max))
+      end
+
+    when "ForeginWorker"
+      if param_value.present?
+        transactions = transactions.where(registration_type: param_value)
+      end
+    when "Registration"
+      if param_value.present?
+        organization_names = Organization.pluck(:name).uniq
+        selected_organization_names = organization_names & param_value
+        transactions = transactions.joins(:organization).where("organizations.name" => selected_organization_names)
+      end
+      # Add more cases for other filter keys here
     end
-    transactions
-
   end
+  transactions
 
-  def displayed_status(status)
-    resp_status = {
-      '1' => "SUCCESS",
-      '0' => "FAILED",
-      '96' => 'IMM BLOCKED',
-      '97' => 'YET TO PROCEED',
-      '98' => "FOREIGN WORKER BLOCKED",
-      "99" => "PHYSICAL NOT DONE"
-    }
-    resp_status[status]
-  end
+end
 
-  def fw_insured(transactions)
-    insurance_purchase_counts = {}
-    transactions.ids.each do |transaction_id|
-      transaction = Transaction.find_by(id: transaction_id)
-      # if transaction
-      #   insurance_purchase_counts[transaction_id] = transaction.foreign_worker.insurance_purchases.count
-      # else
-      #   insurance_purchase_counts[transaction_id] = 0
-      # end
-    end
-    insurance_purchase_counts.values.sum
+def displayed_status(status)
+  resp_status = {
+    '1' => "SUCCESS",
+    '0' => "FAILED",
+    '96' => 'IMM BLOCKED',
+    '97' => 'YET TO PROCEED',
+    '98' => "FOREIGN WORKER BLOCKED",
+    "99" => "PHYSICAL NOT DONE"
+  }
+  resp_status[status]
+end
+
+def fw_insured(transactions)
+  insurance_purchase_counts = {}
+  transactions.ids.each do |transaction_id|
+    transaction = Transaction.find_by(id: transaction_id)
+    # if transaction
+    #   insurance_purchase_counts[transaction_id] = transaction.foreign_worker.insurance_purchases.count
+    # else
+    #   insurance_purchase_counts[transaction_id] = 0
+    # end
   end
+  insurance_purchase_counts.values.sum
+end
 
 
