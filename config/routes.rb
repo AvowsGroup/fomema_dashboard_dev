b82562fdc3897378781183c78adffb0218a29edf
yuvraj <yuvraj.s@avowstech.com>
2023-12-19 18:50:44 +0530

    dashboard 3 filter fixes

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,7 @@ Rails.application.routes.draw do
     namespace :dep_information do
       get 'third_db_data'
       get 'index'
+      get 'filter_kpi'
     end
     get 'customer_satisfaction', to: 'customer_satisfaction#index'
     namespace :customer_satisfaction do
@@ -43,6 +44,27 @@ Rails.application.routes.draw do
         match 'excel_generate', to: 'fw_information#excel_generate', via: [:get, :post], defaults: { format: :xlsx }
       end
     end
+
+    resources :department_information, only: [:new] do
+      collection do
+        post 'calculate_kpi'
+        patch 'update_kpi', to: 'department_information#update_kpi'
+      end
+    end
+
+    resources :department_configuration, except: [:show] do
+      collection do
+        get 'edit', to: 'department_configuration#edit'
+        get 'audit_logs', to: 'department_configuration#audit_logs'
+      end
+    end
+  end
+
+  namespace :settings do
+    get 'customer_survey_upload', to: 'customer_survey_upload#index'
+    post 'customer_survey_upload' => "customer_survey_upload#import"
   end
 
 end
+
+
